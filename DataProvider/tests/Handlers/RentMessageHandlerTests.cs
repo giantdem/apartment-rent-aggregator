31e5baa1f5c0b5f229e6ec122e8f126c74916899
Dmitry Zhomir <giantdem1@gmail.com>
2023-06-27 14:43:25 +0400

    Rent entry test data added

---
+++ DataProvider/tests/Handlers/RentMessageHandlerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataProvider.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProvider.Handlers.Tests
+{
+    [TestClass()]
+    public class RentMessageHandlerTests
+    {
+        [TestMethod()]
+        public void ConsumeTest()
+        {
+            // TODO: implement
+        }
+    }
+}
