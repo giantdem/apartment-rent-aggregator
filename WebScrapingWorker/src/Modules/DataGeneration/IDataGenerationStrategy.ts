11d5e4bec72a84efc1f8b367bb656042685a8d8f
Dmitry Zhomir <giantdem1@gmail.com>
2023-08-07 12:55:53 +0400

    IDataGenerationStrategy

---
+++ WebScrapingWorker/src/Modules/DataGeneration/IDataGenerationStrategy.ts
@@ -0,0 +1,29 @@
+import { IDataGeneration } from './IDataGeneration.js';
+
+// XXX: well, do we actually need this? Or let IDataGeneration have setStrategy()? Or even make these independent?
+export type IDataGenerationStrategy = IDataGeneration;
+
+export class DataGenerationStrategyRegistry
+{
+    private static implementations: (new () => IDataGenerationStrategy)[] = [];
+
+    public static registerImplementation<T extends new () => IDataGenerationStrategy>(
+        ctor: T
+    )
+    {
+        this.implementations.push(ctor);
+        return ctor;
+    }
+
+    public static getImplementations()
+    {
+        return this.implementations.slice();
+    }
+
+    public static getImplementationByName(name: string)
+    {
+        return this.implementations.find(
+            implementation => implementation.name === name
+        );
+    }
+}
