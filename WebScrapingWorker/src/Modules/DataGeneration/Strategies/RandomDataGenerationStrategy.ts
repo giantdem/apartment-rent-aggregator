f2fc772df2f0d7564e054cb918c94721e169e314
Dmitry Zhomir <giantdem1@gmail.com>
2023-08-08 16:08:36 +0400

    faker usage fixed

--- WebScrapingWorker/src/Modules/DataGeneration/Strategies/RandomDataGenerationStrategy.ts
+++ WebScrapingWorker/src/Modules/DataGeneration/Strategies/RandomDataGenerationStrategy.ts
@@ -13,18 +13,18 @@ export class RandomDataGenerationStrategy implements IDataGenerationStrategy
         {
             const rentEntry = new RentEntry();
             rentEntry.Currency = faker.finance.currencyCode();
-            rentEntry.BuildingType = faker.random.arrayElement(['Apartment', 'Townhouse', 'Condo']);
-            rentEntry.Country = faker.address.country();
-            rentEntry.City = faker.address.city();
-            rentEntry.CityArea = faker.address.county();
-            rentEntry.Address = faker.address.streetAddress();
+            rentEntry.BuildingType = faker.helpers.arrayElement(['Apartment', 'Townhouse', 'Condo']);
+            rentEntry.Country = faker.location.country();
+            rentEntry.City = faker.location.city();
+            rentEntry.CityArea = faker.location.county();
+            rentEntry.Address = faker.location.streetAddress();
             rentEntry.Url = `https://example.com/rent/${faker.string.nanoid()}`;
             rentEntry.DateTimePosted = faker.date.recent();
-            rentEntry.Price = faker.random.number({ min: 1000, max: 5000 });
-            rentEntry.IsBuildingModern = faker.random.boolean();
-            rentEntry.FloorNumber = faker.random.number({ min: 1, max: 50 });
-            rentEntry.RoomsAmount = faker.random.number({ min: 1, max: 5 });
-            rentEntry.ApartmentAreaInSqMeters = faker.random.number({ min: 30, max: 200 });
+            rentEntry.Price = faker.number.int({ min: 1000, max: 5000 });
+            rentEntry.IsBuildingModern = faker.datatype.boolean();
+            rentEntry.FloorNumber = faker.number.int({ min: 1, max: 5 });
+            rentEntry.RoomsAmount = faker.number.int({ min: 1, max: 5 });
+            rentEntry.ApartmentAreaInSqMeters = faker.number.int({ min: 30, max: 200 });
             rentEntry.Title = `${rentEntry.Country} - ${rentEntry.City}: ${rentEntry.RoomsAmount} rooms ${rentEntry.BuildingType} for ${rentEntry.Price} ${rentEntry.Currency}`;
 
             rentEntries.push(rentEntry);
