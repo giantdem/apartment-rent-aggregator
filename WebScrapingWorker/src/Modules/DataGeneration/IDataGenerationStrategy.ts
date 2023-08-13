02c6e376f91fd1385a3a38a0267435b6e83862c4
Dmitry Zhomir <giantdem1@gmail.com>
2023-08-13 16:15:25 +0400

    StrategyNotFoundError

--- WebScrapingWorker/src/Modules/DataGeneration/IDataGenerationStrategy.ts
+++ WebScrapingWorker/src/Modules/DataGeneration/IDataGenerationStrategy.ts
@@ -6,7 +6,7 @@ export interface IDataGenerationStrategy
 }
 
 export class DataGenerationStrategyRegistry
-{
+{ // XXX: create an abstract class? extract it to a separate file?
     private static implementations: (new () => IDataGenerationStrategy)[] = [];
 
     public static registerImplementation = <T extends new () => IDataGenerationStrategy>(
@@ -16,6 +16,23 @@ export class DataGenerationStrategyRegistry
 
     public static getImplementations = () => this.implementations.slice();
 
-    public static getImplementationByName = (name: string) =>
-        this.implementations.find(implementation => implementation.name === name);
+    public static getImplementationByName(name: string)
+    {
+        const strategy = this.implementations.find(implementation => implementation.name === name);
+        if (!strategy) throw new StrategyNotFoundError(name);
+        return strategy;
+    }
+}
+
+export class StrategyNotFoundError extends Error
+{
+    constructor(strategyName: string, options: ErrorOptions = {})
+    {
+        const message = `Strategy with name '${strategyName}' not found`;
+
+        super(message, options);
+
+        this.name = this.constructor.name;
+        Error.captureStackTrace?.(this, this.constructor);
+    }
 }
