cd87241758315fd2c3acb4bee2e5900653f735e9
Dmitry Zhomir <giantdem1@gmail.com>
2023-08-20 21:50:26 +0400

    MessagePublishing draft

---
+++ WebScrapingWorker/src/Modules/MessagePublishing/MessagePublishing.ts
@@ -0,0 +1,18 @@
+import { BusInstance } from '@node-ts/bus-core';
+import { IMessagePublishing } from './IMessagePublishing.js';
+import { RentEntry } from '../../Models/RentEntry.js';
+
+export class MessagePublishing implements IMessagePublishing
+{
+    private _bus: BusInstance;
+
+    constructor(bus: BusInstance)
+    {
+        this._bus = bus;
+    }
+
+    async publish(rentEntries: RentEntry[])
+    {
+        await this._bus.publish(rentEntries);
+    }
+}
