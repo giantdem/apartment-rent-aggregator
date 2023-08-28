4762f86b66a99923e0a7bb4c9bf99cf05a75528a
Dmitry Zhomir <giantdem1@gmail.com>
2023-08-28 15:29:53 +0400

    Shorter re-export

--- WebScrapingWorker/src/entities.ts
+++ WebScrapingWorker/src/entities.ts
@@ -1,5 +1,3 @@
-import { DataGeneration } from "./Modules/DataGeneration/DataGeneration.js";
-import { MessagePublishing } from "./Modules/MessagePublishing/MessagePublishing.js";
-import { Orchestrator } from "./Modules/Orchestrator/Orchestrator.js";
-
-export { DataGeneration,  MessagePublishing, Orchestrator };
+export { DataGeneration } from "./Modules/DataGeneration/DataGeneration.js";
+export { MessagePublishing } from "./Modules/MessagePublishing/MessagePublishing.js";
+export { Orchestrator } from "./Modules/Orchestrator/Orchestrator.js";
--- WebScrapingWorker/src/interfaces.ts
+++ WebScrapingWorker/src/interfaces.ts
@@ -1,5 +1,3 @@
-import { IDataGeneration } from "./Modules/DataGeneration/IDataGeneration.js";
-import { IMessagePublishing } from "./Modules/MessagePublishing/IMessagePublishing.js";
-import { IOrchestrator } from "./Modules/Orchestrator/IOrchestrator.js";
-
-export { IDataGeneration,  IMessagePublishing, IOrchestrator };
+export { IDataGeneration } from "./Modules/DataGeneration/IDataGeneration.js";
+export { IMessagePublishing } from "./Modules/MessagePublishing/IMessagePublishing.js";
+export { IOrchestrator } from "./Modules/Orchestrator/IOrchestrator.js";
