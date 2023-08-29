3cfb91d15a1aa226afc3d847ecdb3b91a04c682c
Dmitry Zhomir <giantdem1@gmail.com>
2023-08-29 14:01:38 +0400

    Orchestrator injections

--- WebScrapingWorker/src/Modules/Orchestrator/Orchestrator.ts
+++ WebScrapingWorker/src/Modules/Orchestrator/Orchestrator.ts
@@ -1,11 +1,15 @@
+import { injectable, inject } from "inversify";
+import { TYPES } from "../../types.js";
 import { IDataGeneration,  IMessagePublishing, IOrchestrator } from '../../interfaces.js';
 
+@injectable()
 export class Orchestrator implements IOrchestrator
 {
     private _dataGeneration: IDataGeneration;
     private _messagePublishing: IMessagePublishing;
 
-    constructor(dataGeneration: IDataGeneration, messagePublishing: IMessagePublishing)
+    constructor(@inject(TYPES.IDataGeneration) dataGeneration: IDataGeneration,
+        @inject(TYPES.IMessagePublishing) messagePublishing: IMessagePublishing)
     {
         this._dataGeneration = dataGeneration;
         this._messagePublishing = messagePublishing;
