522a6e4e478d2b89da693dadfe8e915cc876837d
Dmitry Zhomir <giantdem1@gmail.com>
2023-09-07 13:30:26 +0400

    Types updated

--- WebScrapingWorker/src/Modules/MessagePublishing/IMessagePublishing.ts
+++ WebScrapingWorker/src/Modules/MessagePublishing/IMessagePublishing.ts
@@ -2,6 +2,5 @@ import { RentEntry } from '../../Models/RentEntry.js';
 
 export interface IMessagePublishing
 {
-    // XXX: is the type right?
-    publish(rentEntries: RentEntry[]): void;
+    publish(rentEntries: RentEntry[]): Promise<void>;
 }
