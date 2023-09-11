3333d92c8b3a2f64e8b0965299e4046a591622b7
Dmitry Zhomir <giantdem1@gmail.com>
2023-09-11 13:21:50 +0400

    registerMessaging removed

--- WebScrapingWorker/src/inversify.config.ts
+++ WebScrapingWorker/src/inversify.config.ts
@@ -7,34 +7,28 @@ import { Connection, Publisher } from 'rabbitmq-client';
 
 const container = new Container();
 
-registerMessaging();
+const client = new Connection('amqp://guest:guest@localhost');
+client.on('error', (err) => { console.error('RabbitMQ connection error.', err); });
+client.on('connection', () => { console.log('Connection successfully (re)established.'); });
+const pub = client.createPublisher({
+    confirm: true,
+    maxAttempts: 2,
+    exchanges: [{ exchange: 'RentEntries', passive: true }]
+});
+
+container.bind<Publisher>(TYPES.Publisher).toConstantValue(pub);
 container.bind<IDataGeneration>(TYPES.IDataGeneration).to(DataGeneration).inSingletonScope();
 container.bind<IMessagePublishing>(TYPES.IMessagePublishing).to(MessagePublishing).inSingletonScope();
 container.bind<IOrchestrator>(TYPES.IOrchestrator).to(Orchestrator).inSingletonScope();
 
-function registerMessaging()
+function onShutdown()
 {
-    const client = new Connection('amqp://guest:guest@localhost');
-    client.on('error', (err) => { console.error('RabbitMQ connection error.', err); });
-    client.on('connection', () => { console.log('Connection successfully (re)established.'); });
-
-    const pub = client.createPublisher({
-        confirm: true,
-        maxAttempts: 2,
-        exchanges: [{ exchange: 'RentEntries', passive: true }]
-    });
-
-    container.bind<Publisher>(TYPES.Publisher).toConstantValue(pub);
-
-    function onShutdown()
-    {
-        pub.close()
-            .then(() => client.close())
-            .catch((err) => console.error(err));
-        console.log('Messaging connection is closed.');
-    }
-    process.on('SIGINT', onShutdown);
-    process.on('SIGTERM', onShutdown);
+    pub.close()
+        .then(() => client.close())
+        .catch((err) => console.error(err));
+    console.log('Messaging connection is closed.');
 }
+process.on('SIGINT', onShutdown);
+process.on('SIGTERM', onShutdown);
 
 export { container };
